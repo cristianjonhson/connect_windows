809566ae2e0d39d403ace4cd2b0441ea6766bafc
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2019-10-22 14:42:04 -0300

    First C

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace conexionWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String cadenaConexion = "Data source=.; initial catalog=william;integrated security=true";
+                SqlConnection conexionBD = new SqlConnection(cadenaConexion);
+                conexionBD.Open();
+                MessageBox.Show("conexion exitosa");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Conexion Fallida");
+                throw;
+            }
+        }
+    }
+}
